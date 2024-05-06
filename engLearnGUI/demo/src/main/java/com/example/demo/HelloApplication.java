780f42601ac61de6c2390bb6caf5bc5d0d82ac07
Tuan Ngoc T. <105979341+HarlausButtercake@users.noreply.github.com>
2024-05-06 16:35:08 +0700

    foofoo2

--- engLearnGUI/demo/src/main/java/com/example/demo/HelloApplication.java
+++ engLearnGUI/demo/src/main/java/com/example/demo/HelloApplication.java
@@ -10,10 +10,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+//        stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setWidth(800); // Set the width of the stage
+        stage.setHeight(600); // Set the height of the stage
+        stage.setTitle("engLearn");
+        stage.setResizable(false);
         stage.show();
     }
 
